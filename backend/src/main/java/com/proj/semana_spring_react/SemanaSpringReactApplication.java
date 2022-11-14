05f29b3e41940c35764581296b6dc892774e3859
Mariana Regia <marianaregia3@gmail.com>
2022-11-14 18:48:02 -0300

    Projeto criado

---
+++ backend/src/main/java/com/proj/semana_spring_react/SemanaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.proj.semana_spring_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/proj/semana_spring_react/SemanaSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.semana_spring_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanaSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
